c3d5a7b556713b9f7dbd8e313dff20f24d06f562
Sagar Saluja <sagar.saluja@onsolve.com>
2022-12-31 16:20:46 +0530

    problem1 with scrolling backgrounds

--- index.js
+++ index.js
@@ -1,113 +1,51 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
 
 const playerImage = new Image();
-playerImage.src = "Assets/shadow_dog.png";
+playerImage.src = "assets/shadow_dog.png";
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = "Assets/backgroundLayers/layer-1.png";
+backgroundLayer1.src = "assets/backgroundLayers/layer-1.png";
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = "Assets/backgroundLayers/layer-2.png";
+backgroundLayer2.src = "assets/backgroundLayers/layer-2.png";
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = "Assets/backgroundLayers/layer-3.png";
+backgroundLayer3.src = "assets/backgroundLayers/layer-3.png";
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = "Assets/backgroundLayers/layer-4.png";
+backgroundLayer4.src = "assets/backgroundLayers/layer-4.png";
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = "Assets/backgroundLayers/layer-5.png";
+backgroundLayer5.src = "assets/backgroundLayers/layer-5.png";
 
-const frameWidth = 575;
-const frameHeight = 523;
+// const frameWidth = 575;
+// const frameHeight = 523;
 let currentFrameX = 0;
-let currentFrameY = 0;
+// let currentFrameY = 0;
+let gameSpeed = 5;
 
-let gameFrame = 0;
-const staggerFrame = 5;
+// let gameSpeed = 5;
+// let gameFrame = 0;
+// const staggerFrame = 5;
 
-let currentPlayerState = 0;
-const dropDown = document.getElementById("animations");
-dropDown.addEventListener("change", (e) => {
-  currentPlayerState = Number(e.target.value);
-});
-
-const animationLogic = (animationState) => {
-  currentFrameY = animationState.index;
-  if (gameFrame % staggerFrame === 0) {
-    if (currentFrameX < animationState.maxFrame) currentFrameX++;
-    else currentFrameX = 0;
+// const animationLogic = (animationState) => {
+//   currentFrameY = animationState.index;
+//   if (gameFrame % staggerFrame === 0) {
+//     if (currentFrameX < animationState.maxFrame) currentFrameX++;
+//     else currentFrameX = 0;
+//   }
+//   gameFrame++;
+// };
+const drawImage = () => {
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  context.drawImage(backgroundLayer4, currentFrameX, 0);
+  if (currentFrameX < -1000) currentFrameX = 0;
+  else {
+    currentFrameX -= gameSpeed;
   }
-  gameFrame++;
 };
-const animationStates = [
-  {
-    index: 0,
-    type: "idle",
-    maxFrame: 6,
-  },
-  {
-    index: 1,
-    type: "jump",
-    maxFrame: 6,
-  },
-  {
-    index: 2,
-    type: "fall",
-    maxFrame: 6,
-  },
-  {
-    index: 3,
-    type: "run",
-    maxFrame: 8,
-  },
-  {
-    index: 4,
-    type: "dizzy",
-    maxFrame: 10,
-  },
-  {
-    index: 5,
-    type: "sit",
-    maxFrame: 4,
-  },
-  {
-    index: 6,
-    type: "roll",
-    maxFrame: 6,
-  },
-  {
-    index: 7,
-    type: "bite",
-    maxFrame: 6,
-  },
-  {
-    index: 8,
-    type: "ko",
-    maxFrame: 11,
-  },
-  {
-    index: 9,
-    type: "get hit",
-    maxFrame: 3,
-  },
-];
-
-const drawImage = () =>
-  context.drawImage(
-    playerImage,
-    currentFrameX * frameWidth,
-    currentFrameY * frameHeight,
-    frameWidth,
-    frameHeight,
-    0,
-    0,
-    frameWidth,
-    frameHeight
-  );
 
 const animate = () => {
-  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  animationLogic(animationStates[currentPlayerState]);
+  // animationLogic(animationStates[currentPlayerState]);
   drawImage();
   requestAnimationFrame(animate);
 };
